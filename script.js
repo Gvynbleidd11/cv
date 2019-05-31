9231090d94fd03b1b25021540dd3748a6a46bd59
Gvynbleidd11 <51246124+Gvynbleidd11@users.noreply.github.com>
2019-05-31 21:29:44 +0200

    Add initial cv website files

---
+++ script.js
@@ -0,0 +1,10 @@
+﻿var dzisiaj = new Date();
+
+var dzien = dzisiaj.getDate();
+var miesiac = dzisiaj.getMonth()+1;
+var rok = dzisiaj.getFullYear();
+
+var godzina = getHours();
+var minuta = dzisiaj.getMinutes();
+var sekunda = dzisiaj.getSeconds();
+
